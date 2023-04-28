ed2192f9aea34d7d1e109c21c49380baecf61750
Isha Slavin <Isha.Slavin@mheducation.com>
2023-04-28 15:34:57 -0400

    fixed data args and training args

--- examples/pytorch/question-answering/NEW_apr28_run100dp_custom_finetuning.py
+++ examples/pytorch/question-answering/NEW_apr28_run100dp_custom_finetuning.py
@@ -708,11 +708,14 @@ def main():
     training_args.generation_max_length = (
         training_args.generation_max_length
         if training_args.generation_max_length is not None
-        else data_args.val_max_target_length
+        # else data_args.val_max_target_length
+        else data_args.max_answer_length
     )
-    training_args.generation_num_beams = (
-        data_args.num_beams if data_args.num_beams is not None else training_args.generation_num_beams
-    )
+    # training_args.generation_num_beams = (
+    #     data_args.num_beams if data_args.num_beams is not None else training_args.generation_num_beams
+    # )
+    ''' added. '''
+    # training_args.generation_num_beams = training_args.generation_num_beams
 
     # Initialize our Trainer
     print('*** Initializing TRAINER.')
